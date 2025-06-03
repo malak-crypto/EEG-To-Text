4124beabd867be0a3ce45a59d25a748fe01089d3
Malak Khaled <68809008+malak-crypto@users.noreply.github.com>
2025-06-03 04:49:58 +0300

    Update prepare_dataset.sh

--- scripts/prepare_dataset.sh
+++ scripts/prepare_dataset.sh
@@ -6,7 +6,8 @@ echo "Note: This process can take time, please be patient..."
 python3 ./util/construct_dataset_mat_to_pickle_v1.py -t task1-SR
 python3 ./util/construct_dataset_mat_to_pickle_v1.py -t task2-NR
 python3 ./util/construct_dataset_mat_to_pickle_v1.py -t task3-TSR
-python3 ./util/construct_dataset_mat_to_pickle_v2.py
+#python3 ./util/construct_dataset_mat_to_pickle_v2.py
+#had to do another .py file with memory optimization and better handling in case the job got terminated
 python3 ./util/construct_dataset_mat_to_pickle_memory.py
 
 #for my masters i am not going to use zero shot sentiment classification
