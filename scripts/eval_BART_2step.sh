e91b52bd544685de33ad9b1803d9264ffa33a809
Malak Khaled <68809008+malak-crypto@users.noreply.github.com>
2025-07-08 01:03:15 +0300

    Update eval_BART_2step.sh

--- scripts/eval_BART_2step.sh
+++ scripts/eval_BART_2step.sh
@@ -21,8 +21,8 @@ conda activate /home/w314/w314139/.conda/envs/EEGToText
 echo "Starting training at: $(date)"
 
 python3 eval_decoding.py \
-    -checkpoint checkpoints/decoding/best/task1_task2_taskNRv2_finetune_BrainTranslator_skipstep1_b32_20_30_2e-05_2e-05_unique_sent_EEG.pt \
-    -conf config/decoding/task1_task2_taskNRv2_finetune_BrainTranslator_skipstep1_b32_20_30_2e-05_2e-05_unique_sent_EEG.json \
+    -checkpoint checkpoints/decoding/best/task1_task2_taskNRv2_finetune_BrainTranslator_2steptraining_b32_20_30_2e-05_2e-05_unique_sent_EEG.pt \
+    -conf config/decoding/task1_task2_taskNRv2_finetune_BrainTranslator_2steptraining_b32_20_30_2e-05_2e-05_unique_sent_EEG.json \
     -test_input EEG \
     -train_input EEG \
     -cuda cuda:0
